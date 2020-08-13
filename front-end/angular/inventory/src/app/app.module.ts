04437ae77fe74c0bc28acf6c1d469cd2233887b1
Steff Beckers <steff@steffbeckers.eu>
2020-08-14 01:33:32 +0200

    Retrieve weather forecast in angular project

--- front-end/angular/inventory/src/app/app.module.ts
+++ front-end/angular/inventory/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { API_BASE_URL } from 'src/api/inventory.api';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    {
+      provide: API_BASE_URL,
+      useValue: environment.api_base_url,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
