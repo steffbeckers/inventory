c21b3ab8c30fc268e12937be510efb4c0fc11e2d
Steff Beckers <steff@steffbeckers.eu>
2020-08-17 00:53:06 +0200

    OIDC config for API

--- back-end/dotnet/Inventory/src/Infrastructure/DependencyInjection.cs
+++ back-end/dotnet/Inventory/src/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Inventory.Application.Common.Interfaces;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using Inventory.Application.Common.Interfaces;
 using Inventory.Infrastructure.Files;
 using Inventory.Infrastructure.Identity;
 using Inventory.Infrastructure.Persistence;
@@ -7,6 +9,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 
 namespace Inventory.Infrastructure
 {
@@ -25,7 +28,23 @@ namespace Inventory.Infrastructure
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(options => {
+                    options.Clients.Add(new Client()
+                    {
+                        ClientId = "angular",
+                        ClientName = "Angular app",
+                        RequireClientSecret = false,
+                        AllowedGrantTypes = GrantTypes.ImplicitAndClientCredentials,
+                        AllowAccessTokensViaBrowser = true,
+                        AllowedScopes = new List<string>() {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile
+                        },
+                        // TODO: Configurable
+                        AllowedCorsOrigins = new List<string>() { "http://localhost:4200" },
+                        RedirectUris = new List<string>() { "http://localhost:4200/auth/oidc-callback" }
+                    });
+                });
 
             services.AddTransient<IDateTime, DateTimeService>();
             services.AddTransient<IIdentityService, IdentityService>();
