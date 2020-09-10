2aeb6e2805135ba93c5b1151a60f9bd46f0eaac5
Steff Beckers <steff@steffbeckers.eu>
2020-09-10 22:33:30 +0200

    Updates on theming with light and dark theme

--- front-end/angular/inventory/src/app/app.component.ts
+++ front-end/angular/inventory/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.document.documentElement.classList.add('dark-theme');
+  }
 }
